83e7b6f85a5cae074a59e78964dc62b04adb05f6
JulianEichen <juleichen@aol.com>
2023-02-11 13:25:23 +0100

    basic layout, strcuture and register page

--- flaskblog.py
+++ flaskblog.py
@@ -1,20 +1,51 @@
 # save this as app.py
-from flask import Flask, request
-from markupsafe import escape
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegistrationForm, LoginForm
+
 
 app = Flask(__name__)
+
+app.config['SECRET_KEY']='a343c940d73bd8707d351de0db9c6da6'
+
+posts=[
+    {
+        'author': 'Julian Eichen',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'July 4, 2044'
+    },
+    {
+        'author': 'Jane Doe',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'July 22, 2044'
+    }
+]
 
 @app.route('/') # home page
 @app.route('/home') 
 def home():
-    name = request.args.get("name", "Home")
-    return f'<h1>{escape(name)} Page!</h1>'
+    return render_template('home.html', posts=posts)
 
-@app.route('/about') # home page
+@app.route('/about') # about page
 def about():
-    name = request.args.get("name", "About")
-    return f'<h1>{escape(name)} Page!</h1>'
+    return render_template('about.html',title='About')
+
+@app.route('/register', methods=['GET','POST'])
+def register():
+    form=RegistrationForm()
+    if form.validate_on_submit():
+        flash(f"Account created for {form.username.data}!", "success")
+        return redirect(url_for('home'))
+    return render_template('register.html',title='Register', form=form)
+
+@app.route('/login')
+def login():
+    form=LoginForm()
+    return render_template('login.html',title='Login', form=form)
+
 
 
 if __name__ == "__main__":
     app.run(debug=True)
+
