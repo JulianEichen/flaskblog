899725a511218298b4aa6c0f791067b3b56c8d61
JulianEichen <juleichen@aol.com>
2023-02-13 14:19:17 +0100

    first part of user authentication

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -1,9 +1,13 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_loing import LoginManager
 
 app = Flask(__name__)
 app.config['SECRET_KEY']='a343c940d73bd8707d351de0db9c6da6'
 app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///site.db'
 db= SQLAlchemy(app)
+bcrypt=Bcrypt(app)
+login_manager=LoginManager(app)
 
 from flaskblog import routes # need to avoid circular import problems
--- flaskblog/forms.py
+++ flaskblog/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
-from wtforms.validators import DataRequired, Length, Email, EqualTo
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from flaskblog.models import User
 
 class RegistrationForm(FlaskForm):
     username=StringField('Username', 
@@ -15,6 +16,25 @@ class RegistrationForm(FlaskForm):
                         validators=[DataRequired(),EqualTo('password')])
 
     submit=SubmitField('Sign Up')
+
+    # custom validations
+    
+    # Template:
+    # def validate_field(self, field):
+    #    if True:
+    #        raise ValidationError('Validation Message')
+
+    def validate_username(self, username):
+        user=User.query.filter_by(username=username.data).first()
+        if user:
+            raise ValidationError('Username already exists.') 
+
+    def validate_email(self, email):
+        user=User.query.filter_by(email=email.data).first()    
+        if user:
+            raise ValidationError('Email already in use.') 
+
+    
 
 class LoginForm(FlaskForm):
     email=StringField('email',
