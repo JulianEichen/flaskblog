279bb00d05eaeb870ac1a022e3e8f7ece7e58457
JulianEichen <juleichen@aol.com>
2023-02-13 10:16:27 +0100

    login&register frontend

--- flaskblog.py
+++ flaskblog.py
@@ -1,11 +1,16 @@
 # save this as app.py
 from flask import Flask, render_template, url_for, flash, redirect
 from forms import RegistrationForm, LoginForm
+from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
 
 app.config['SECRET_KEY']='a343c940d73bd8707d351de0db9c6da6'
+app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///site.db'
+
+db= SQLAlchemy(app)
+
 
 posts=[
     {
@@ -39,9 +44,15 @@ def register():
         return redirect(url_for('home'))
     return render_template('register.html',title='Register', form=form)
 
-@app.route('/login')
+@app.route('/login',methods=['GET','POST'])
 def login():
     form=LoginForm()
+    if form.validate_on_submit():
+        if form.email.data== 'admin@blog.com' and form.password.data =='123':
+            flash("You have been logged in!", 'success')
+            return redirect(url_for('home'))
+        else:
+            flash("Log unsuccessful. Please check username and password.", 'danger')
     return render_template('login.html',title='Login', form=form)
 
 
