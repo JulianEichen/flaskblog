4eda73757531e41aa120f5a1d2433a1297f6edd8
JulianEichen <juleichen@aol.com>
2023-02-15 11:23:42 +0100

    create posts front&back

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -3,28 +3,14 @@ import secrets
 from PIL import Image
 from flask import render_template, url_for, flash, redirect, request
 from flaskblog import app, db, bcrypt
-from flaskblog.forms import RegistrationForm, LoginForm, UpdateAccountForm
+from flaskblog.forms import RegistrationForm, LoginForm, UpdateAccountForm, PostForm
 from flaskblog.models import User, Post
 from flask_login import login_user, logout_user, current_user, login_required
-
-posts=[
-    {
-        'author': 'Julian Eichen',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': 'July 4, 2044'
-    },
-    {
-        'author': 'Jane Doe',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted': 'July 22, 2044'
-    }
-]
 
 @app.route('/') # home page
 @app.route('/home') 
 def home():
+    posts=Post.query.all()
     return render_template('home.html', posts=posts)
 
 @app.route('/about') # about page
@@ -58,7 +44,7 @@ def login():
             next_page=request.args.get('next')
             return redirect(next_page) if next_page else redirect(url_for('home'))
         else:
-            flash("Log unsuccessful. Please check username and password.", 'danger')
+            flash("Login unsuccessful. Please check username and password.", 'danger')
     return render_template('login.html',title='Login', form=form)
 
 @app.route('/logout')
@@ -97,3 +83,16 @@ def account():
         form.email.data=current_user.email
     image_file=url_for('static', filename='profile_pics/'+current_user.image_file)
     return render_template('account.html', title='Account', image_file=image_file, form=form)
+
+
+@app.route('/post/new',methods=['GET','POST'])
+@login_required
+def new_post():
+    form=PostForm()
+    if form.validate_on_submit():
+        post=Post(title=form.title.data,content=form.content.data, author=current_user)
+        db.session.add(post)
+        db.session.commit()
+        flash('Your post has been created!','success')
+        return redirect(url_for('home'))
+    return render_template('create_post.html', title='New Post',form=form)
