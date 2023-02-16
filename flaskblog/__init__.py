4dd575e48a464f07633c489f61fa0a62740331d1
JulianEichen <juleichen@aol.com>
2023-02-16 13:08:33 +0100

    reset password by email front

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -2,14 +2,18 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+from flask_mail import Mail
 
 app = Flask(__name__)
 app.config['SECRET_KEY']='a343c940d73bd8707d351de0db9c6da6'
 app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///site.db'
 db= SQLAlchemy(app)
 bcrypt=Bcrypt(app)
+
 login_manager=LoginManager(app)
 login_manager.login_view='login'
 login_manager.login_message_category='info'
+
+app.config['MAIL_SERVER']
 
 from flaskblog import routes # need to avoid circular import problems
